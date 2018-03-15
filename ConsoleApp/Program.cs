782926a83d0c1d31186b33d91d72c213cf53d733
ybiren <37359767+ybiren@users.noreply.github.com>
2018-03-15 11:13:31 +0200

    Add files via upload

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Script.Serialization;
+using System.Web.Script.Serialization; //System.Runtime.Serialization
 using System.Net;
 
 namespace ConsoleApp
